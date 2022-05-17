55b699afc628948337faa0530974073d90b64956
cristianjofre <megalocoman@gmail.com>
2022-05-17 19:54:58 -0400

    more biding

--- src/app/server-element/server-element.component.ts
+++ src/app/server-element/server-element.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, ContentChild, ElementRef, Input, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -7,14 +7,30 @@ import { Component, Input, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.Emulated
   // None, Native, Emulated
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit  {
 
 
+  @ViewChild('heading',{static:true}) header: ElementRef;
+  @ContentChild('contentParagraph',{static:true}) paragraph : ElementRef;
   @Input('srvElement') element:{type: string, name: string, content: string};
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('text content: '+this.header.nativeElement.textContent);
+    console.log('text content of paragraph: '+this.paragraph.nativeElement.textContent);
+
+
+  }
+
+  ngAfterViewInit(){
+
+    console.log('ngAfterViewInit called');
+  }
+
+  ngAfterContentChecked(){
+    console.log('ngAfterContentChecked called');
+    console.log('text content of paragraph: '+this.paragraph.nativeElement.textContent);
   }
 
 }
